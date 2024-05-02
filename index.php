077fe32fee4c5f2709fb8e56933dba3393ee01b4
DamianoSpampinato <ghgg585@gmail.com>
2024-05-02 17:04:32 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+    ];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php hotel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <!-- prendo le chiavi dinamicamente  -->
+            <?php foreach($hotels[0] as $key => $value) {?>
+                <th scope="col"><?php echo str_replace('_',' ', ucfirst($key)) ?></th>
+            <?php } ?>
+            </tr>
+        </thead>
+        <tbody>
+            <!-- scorro gli hotel -->
+        <?php foreach ($hotels as $hotel) {?>
+        <tr>
+            <!-- stampo le informzioni corrette dopo la verifica -->
+            <?php foreach($hotel as $key => $value) { ?>
+                <?php if ($key === "parking"){ ?>
+                    <td><?php echo $value ? "Present" : "None" ?></td>
+                <?php } elseif ($key === "distance_to_center"){ ?>
+                    <td><?php echo $value ?>km</td>
+                <?php } else { ?>
+                    <td><?php echo $value ?></td>
+                <?php } ?>
+            <?php } ?>
+        </tr>
+        <?php } ?>
+    </tbody>
+    </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
